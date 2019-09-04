1bfa6e3e033bdb256464f3b30cfa3428ecf2ef25
Jon Reades <jon@reades.com>
2019-09-04 14:22:24 +0100

    Sketch code to create a template conjuring project from some 'clean' source directory.

--- src/project.py
+++ src/project.py
@@ -1,32 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This script is used to configure Conjuring for a deployment.
-# Depending on the parameters entered you will either enter an
-# interactive mode or will have conjuring prepped for deployment
+# This script is used to configure Conjuring.
+# Depending on the parameters entered you either enter an
+# interactive mode or will have conjuring prepped 
 # using your specified options.
 
 import os
 import csv 
 import string
-import secrets
+import shutil
 import argparse
 
-# Where to find Conjuring's custom directory.
-custom_dir = 'custom'
+HOME_PATH = '/Users/jreades/Desktop/conjuring'
 
 parser=argparse.ArgumentParser(
     description='''
-    This script is used to configure Conjuring for a deployment.
-    Depending on the parameters entered you will either enter an
-    interactive mode or will have conjuring prepped for deployment
-    using your specified options.
+    Create a new Conjuring project with a default template.
     ''',
     epilog="""Please see https://github.com/conjuring/conjuring for more help/to report issues.""")
-parser.add_argument('--num', type=int, default=20, help='The number of users to create (defaults to 20).')
+
+# Look up how to allow --force to work instead of --force True
+parser.add_argument('--dest', type=str, default="conjuring", help='Overrides the default \'destination\' directory for creating the template')
+parser.add_argument('--force', type=bool, default=False, help='Force overwriting of existing destination directory by deleting and recreating.')
 
 args=parser.parse_args()
 
-print(args.num)
+# Create destination directory from --dest
+# - Check if directory exists
+# - Check if not empty
+# - Exit
+target_dir = os.path.join(os.getcwd(),args.dest)
 
-exit()
+if args.force is True and os.path.isdir(target_dir):
+    print("Not yet implemented.")
+    pass 
+
+if os.path.isdir(target_dir):
+    print(f"'{args.dest}' directory already exists in {os.getcwd()}")
+    exit(1)
+else:
+    print(f"Creating target {target_dir}")
+
+    # Once installed via pip may need to check
+    # permissions in source and destination
+    #os.makedirs(target_dir, mode=0o755)
+    
+    shutil.copytree(HOME_PATH, target_dir)
+
+exit(0)
