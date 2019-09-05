2c3f1102692ea72663a6495500b5b20e0dc44482
Jon Reades <jon@reades.com>
2019-09-05 14:03:58 +0200

    AWK much better.

--- update-ip.sh
+++ update-ip.sh
@@ -5,14 +5,14 @@
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-IP_ADDR=`ifconfig | grep 'inet ' | grep -v -E '172\.|127\.' | cut -d' ' -f10 | tr '\n', ','`
-DATE=`/bin/date`
+IP_ADDR=$(ifconfig | awk '/inet[^6]/ {print $2}' | grep -v "1[27][27]" | tr '\n', ',')
+DATE=$(date)
 
-IP_ADDR=$(echo "$IP_ADDR" | sed 's|,|</br>\n|g')
+IP_ADDR=$(echo "$IP_ADDR" | sed 's|,|</br>|g')
 
-cat head.html > tmp.html
-printf "<h3>$IP_ADDR</h3>\n<h3>Last Updated: $DATE</h3>\n" >> "tmp.html"
-cat foot.html >> tmp.html
+printf "<html><title>Current NUC IP Addresses</title><body>\n" > tmp.html
+printf "<h3>$IP_ADDR</h3>\n<h3>Last Updated: $DATE</h3>"$'\n' >> tmp.html
+printf "</body></html>" >> tmp.html
 
-scp tmp.html conjuring@reades.com:public_html/index.html
+#scp tmp.html conjuring@reades.com:public_html/index.html
 
